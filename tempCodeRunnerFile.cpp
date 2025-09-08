7455bd63142bf49a237c7c990f8de13aa3b339b4
mehediahmmad <mehediahmmadrodro@gmail.com>
2025-09-08 22:44:33 +0600

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-// using namespace std;
-// int main(){
-//     int a,b=0;
-//     cin>>a;
+using namespace std;
+int main(){
+    int a,b=0;
+    cin>>a;
 
-//     for(int i=1;i<=a;i++){
-//         (i%2 == 0)?b=a/2:b=-(a+1)/2;  
-//     }
-//     cout <<b<<endl;
+    for(int i=1;i<=a;i++){
+        (i%2 == 0)?b=a/2:b=-(a+1)/2;  
+    }
+    cout <<b<<endl;
 
-//     return 0;
-// }
+    return 0;
+
+}
